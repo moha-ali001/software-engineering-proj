9d95fb769ddb0aae48d35d0329bfd9f061fcbaa2
pfishe13 <76025363+pfishe13@users.noreply.github.com>
2021-04-06 16:57:47 -0400

    Calories Page (#51)
    * Update setup.py
    
    * Update swift.py
    
    * Update and rename calories.tpl to meals.tpl
    
    * changed url from calories to meals
    
    * changed button in menu to read meals instead of calories

--- setup.py
+++ setup.py
@@ -11,3 +11,10 @@ if __name__ == "__main__":
         {"time":0.3, "sets":"3", "reps":"15", "description":"Incline Chest Fly", "list":"tomorrow", "completed":False},
         {"time":0.7, "sets":"5", "reps":"10", "description":"Pushups", "list":"tomorrow", "completed":True}
     ]) 
+    meal_table = taskbook_db.get_table('meal')
+    meal_table.drop()
+    meal_table = taskbook_db.create_table('meal')
+    meal_table.insert_many([
+        {"time":0.0, "food":"Apple", "amount":"1", "calories":52,"list":"today", "completed":False},
+        {"time":0.5, "food":"Grilled Chicken", "amount":"3 ounces", "calories":160, "list":"today", "completed":False},
+    ]) 
